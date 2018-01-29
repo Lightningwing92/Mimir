d7ba5b6d113de2e43c48de1e6fe6d694cbee98cb
Lightningwing92 <lightningwing92@gmail.com>
2018-01-29 06:43:52 -0700

    Added HTMLAgilityPack library, and finalized codex query command.

--- Mimir/Mimir.Modules/CanonnCodexQuery.cs
+++ Mimir/Mimir.Modules/CanonnCodexQuery.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Discord.Commands;
 using Discord;
 using Newtonsoft.Json.Linq;
+using HtmlAgilityPack;
 
 namespace Mimir.Mimir.Modules
 {
     public class CanonnCodexQuery : ModuleBase
     {
-        [Command("query")]
+        [Command("codex")]
         [Summary("Queries the Canonn Codex for any relevant articles")]
         public async Task codexQuery (string query)
         {
@@ -52,13 +51,25 @@ namespace Mimir.Mimir.Modules
             JArray entries = JArray.Parse(apiJson);
 
             string CodexLink = entries[0]["link"].ToString();
-            string CodexTitle = entries[0]["title"]["rendered"].ToString();
-            string CodexExcerpt = entries[0]["excerpt"]["rendered"].ToString();
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(entries[0]["title"]["rendered"].ToString());
+            string CodexTitle = WebUtility.HtmlDecode(doc.DocumentNode.FirstChild.InnerHtml);
+            doc.LoadHtml(entries[0]["excerpt"]["rendered"].ToString());
+            string CodexExcerpt = WebUtility.HtmlDecode(doc.DocumentNode.FirstChild.InnerHtml);
+
+            string FeaturedImageURL = WebUtility.HtmlDecode(entries[0]["_links"]["wp:featuredmedia"][0]["href"].ToString());
+            string mediaContent;
+            using (WebClient client = new WebClient())
+            {
+                mediaContent = client.DownloadString(FeaturedImageURL);
+            }
+            JObject mediaJson = JObject.Parse(mediaContent);
 
             var embed = new EmbedBuilder();
             embed.WithTitle(CodexTitle);
             embed.WithUrl(CodexLink);
             embed.AddField("Summary", CodexExcerpt);
+            embed.WithThumbnailUrl(WebUtility.HtmlDecode(mediaJson["guid"]["rendered"].ToString()));
 
             await Context.Channel.SendMessageAsync("", false, embed);
         }
